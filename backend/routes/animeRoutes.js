235158cb8e29016ccaff98f17ab9e5c61fdc7e05
TSDPRASAD88 <prasad.thamarana2006@gmail.com>
2025-05-27 17:58:21 +0530

    GET done sucessfully

--- backend/routes/animeRoutes.js
+++ backend/routes/animeRoutes.js
@@ -1,5 +1,5 @@
 // animeRoutes.js
-
+// animeRoutes
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+// homepage
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../Navbar';
 import '../HomePage.css';
